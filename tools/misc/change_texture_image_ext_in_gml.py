1791c018ffe1fd2f082115c0237c483297b0c146
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2025-02-26 00:31:19 +0900

    ファイル名変更

--- tools/misc/change_texture_image_ext_in_gml.py
+++ tools/misc/change_texture_image_ext_in_gml.py
@@ -4,7 +4,7 @@ from lxml import etree
 import argparse
 
 
-def change(input: str, output: str, format: str) -> None:
+def change(input: str, output: str, ext: str) -> None:
     """
     GMLファイルを解析し、`app:ParameterizedTexture` 要素内の `imageURI` の
     ファイル拡張子を指定されたものに更新し、修正後のXMLを新しいファイルに書き出します。
@@ -14,8 +14,8 @@ def change(input: str, output: str, format: str) -> None:
     :type input: str
     :param output: 修正されたGMLファイルを書き出す出力パス。有効なファイルパスである必要があります。
     :type output: str
-    :param format: `imageURI`の古い拡張子を置き換えるための新しいファイル拡張子。
-    :type format: str
+    :param ext: `imageURI`の古い拡張子を置き換えるための新しいファイル拡張子。
+    :type ext: str
     :return: この関数は値を返しません。修正された内容は指定されたファイルに保存されます。
     :rtype: None
     """
@@ -33,7 +33,7 @@ def change(input: str, output: str, format: str) -> None:
             if parameterized_texture is not None:
                 image_uri = parameterized_texture.find('app:imageURI', namespaces)
                 if image_uri is not None and image_uri.text:
-                    image_uri.text = str(Path(image_uri.text).with_suffix(f'.{format}'))
+                    image_uri.text = str(Path(image_uri.text).with_suffix(f'.{ext}'))
 
     # 結果を新しいファイルに保存 (XML宣言を含む)
     tree.write(output, encoding='utf-8', xml_declaration=True, pretty_print=True)
@@ -45,13 +45,13 @@ def main():
     parser.add_argument("-i", "--input", help="Input GML file")
     parser.add_argument("-o", "--output", help="Output GML file")
     parser.add_argument(
-      '--format', type=str, default='png', help='Converted image extension'
+      '--ext', type=str, default='png', help='Converted image extension'
     )
 
     args = parser.parse_args()
 
     # 実行
-    change(args.input, args.output, args.format)
+    change(args.input, args.output, args.ext)
 
 
 if __name__ == "__main__":
