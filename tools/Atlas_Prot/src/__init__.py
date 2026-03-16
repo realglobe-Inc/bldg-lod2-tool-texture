bec11b7ce9d0f7f08a2611a6dec063bc258109fe
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2026-03-17 05:05:17 +0900

    使っていないツールを削除

--- tools/Atlas_Prot/src/__init__.py
+++
@@ -1,1 +0,0 @@
-from .AtlasTextureMain import main
--- tools/Atlas_Prot/src/stey_mesh.py
+++
@@ -1,7 +0,0 @@
-class SetyMesh:
-    """4次メッシュ情報クラス"""
-
-    def __init__(self) -> None:
-        """コンストラクタ"""
-        self.ids = list()
-        self.code = 0
